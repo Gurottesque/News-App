da919a0a1ef9de637f92c2cef7e7ab1ba87baf18
Jeffer512 <140467995+Jeffer512@users.noreply.github.com>
2024-06-30 01:28:26 -0500

    Agregadas funciones para manejar paginacion (solo botones next y previous) a Home.jsx y SearchPage.jsx

--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -30,11 +30,35 @@ function SearchBar() {
 }
 
 function SearchResults() {
+    // Funcion para mostrar los tres resultados siguientes
+    const nextPage = () => {
+        if(index < 98){
+            setIndex(index+3);
+        }else{
+            setIndex(0);
+            setPage(page+1)
+        }
+    }
+    
+    // Funcion para mostrar los tres resultados anteriores
+    const previousPage = () => {
+        if(index === 0 && page === 1){
+            return;
+        }
+        else if(index > 2){
+            setIndex(index-3);
+        }else{
+            setIndex(99);
+            setPage(page-1)
+        }
+    }
     return (
         <div className="flex flex-col md:flex-row justify-center items-center md:space-x-2 space-y-2 md:space-y-0 p-4 gap-4">
             <Card />
             <Card />
             <Card />
+            <button onClick={nextPage}>Next</button>
+            <button onClick={previousPage}>Previous</button>
         </div>
       );
 }
@@ -57,10 +81,10 @@ const Card = () => {
             </div>
         </article>
     </section>
-    
-    
+
     );
   };
+
 
 function SearchPage() {
     return (
