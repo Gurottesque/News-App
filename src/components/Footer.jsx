6eac64b9c653375721a39dfa1a308b3251e5a7e3
FrankiNarvaez <frannki03@gmail.com>
2024-06-30 06:36:05 -0500

    Unido la pagina de inicio, union de componentes en la pagina de busqueda

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,47 +1,32 @@
 import React, { useState } from "react";
 import Modal from "./Modal";
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 function Footer() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   return (
     <>
-      <footer className="bg-gray-100 text-gray-500 py-4">
-        <div className="container mx-auto px-4 flex flex-col md:flex-row items-center justify-between">
-          <div className="flex space-x-6 mb-4 md:mb-0">
-            <a href="#about" onClick={() => setIsModalOpen(true)}>About</a>
-            <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
-            <a href="/contact" className="hover:text-sky-700">
-              Contact       
-            </a>
-            <a href="/privacy" className="hover:text-sky-700">
-              Privacy
-            </a>
+      <footer class="bg-gray-100 text-gray-500 py-4">
+        <div class="container mx-auto px-4 flex flex-col md:flex-row items-center justify-between">
+          <div class="flex space-x-6 mb-4 md:mb-0">
+          <a href="#" onClick={() => setIsModalOpen(true)}>About</a>
+          <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
+            <a class="hover:text-sky-700">Contact</a>
+            <a class="hover:text-sky-700">Privacy</a>
           </div>
-          <div className="text-center mb-4 md:mb-0">
+          <div class="text-center mb-4 md:mb-0">
             &copy; 2024 News Hub. All rights reserved.
           </div>
-          <div className="mt-4 flex space-x-6">
-            <a>
-              <img
-                src="https://store-images.s-microsoft.com/image/apps.37935.9007199266245907.b029bd80-381a-4869-854f-bac6f359c5c9.91f8693c-c75b-4050-a796-63e1314d18c9?h=210"
-                alt="Facebook"
-                className="h-6 w-6"
-              />
-            </a>
-            <a>
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlg_jM9FuLfEnjTvQcvYC5XGbLkEm5pKE-Ng&s"
-                alt="X"
-                className="h-6 w-6"
-              />
-            </a>
-            <a>
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1200px-Instagram_logo_2022.svg.png"
-                alt="Instagram"
-                className="h-6 w-6"
-              />
-            </a>
+          <div class="mt-4 flex space-x-6">
+              <a href="#" className="hover:text-gray-700">
+                  <FaFacebook className="h-6 w-6" />
+                </a>
+                <a href="#" className="hover:text-gray-700">
+                  <FaTwitter className="h-6 w-6" />
+                </a>
+                <a href="#" className="hover:text-gray-700">
+                  <FaInstagram className="h-6 w-6" />
+                </a>
           </div>
         </div>
       </footer>
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,6 +1,4 @@
-import Navbar from "./Navbar"
-import Footer from "./Footer"
-
+import Card from "./Card";
 
 const topCurrentNews = [
     {
@@ -42,20 +40,30 @@ function MainPage(){
     }
     return (
     <>
-        <button onClick={nextPage}>Next</button>
-        <button onClick={previousPage}>Previous</button>
+    
+        <h1 className="flex items-center justify-center mt-5 text-5xl font-bold">Breaking News</h1>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-4 max-w-screen-2xl m-auto">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+        </div>
+        <div className="flex justify-center gap-20 mb-5">
+            <button className="bg-black text-white rounded-md px-5 py-2 hover:bg-slate-300 hover:text-black transition-colors" onClick={previousPage}>Previous</button>
+            <button className="bg-black text-white rounded-md px-5 py-2 hover:bg-slate-300 hover:text-black transition-colors" onClick={nextPage}>Next</button>
+        </div>
     </>
     );
 }
 
-function Home(){
-
-    return 
-    <>
-        <Navbar />
+function Home() {
+  return(
+    <> 
         <MainPage />
-        <Footer />
     </>
+  );
 }
 
 export default Home;
