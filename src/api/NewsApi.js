b120c2b7706a800189ceabf79e796a6cc119ea3b
Gurottesque <a.bedoya3@utp.edu.co>
2024-06-29 23:01:48 -0500

    Reemplazo del archivo api

---
+++ src/api/NewsApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+
+const API_KEY = '902c9441-a973-4483-ae9d-cee0a03f2bb8'
+export const NewsApi = createApi({
+    reducerPath: 'api',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://eventregistry.org/api/v1/'
+    }),
+
+    endpoints: (builder) => ({
+
+
+        getTrendingHome: builder.query({
+            query: () => `/event/getBreakingEvents?apiKey=${API_KEY}`
+        })
+    }),
+
+
+})
+
+
+export const { useGetTrendingHomeQuery } = NewsApi;
