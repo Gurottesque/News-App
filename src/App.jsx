6b539571eba37dc6fa85205ea8b97a5e386bee23
CamiloMunozAL <camilo.munoz2@utp.edu.co>
2024-06-30 02:08:31 -0500

    Agregando enrutado inicial y librerias

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import Home from "./components/HomePage"
+import Home from "./components/HomePage";
+import SearchPage from "./components/SearchPage";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
     <>
-     <Routes>
-        <Route path = '/' element = {<Home/>} /> 
-        <Route path = '/search' element = {<SearchPage />}/>
-      </Routes>    
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<SearchPage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
