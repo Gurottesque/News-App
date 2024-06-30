c7ed60c293a5d113e88e1a37fac311fd3a494da0
Esteban <160088637+esteban23052005@users.noreply.github.com>
2024-06-30 01:32:45 -0500

    Se agrego el SearchBar y el CategorySelection en SearchPag ademas de los tailwind

--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -17,32 +17,58 @@ const searchResultsArray = [
 ]
 
 function CategorySelection() {
-    return
-    <>
-    </>
+    return(
+        <div className="flex flex-col items-center">
+            <h2 className="text-lg font-bold mb-2">Filter</h2>
+            <label htmlFor="category" className="mb-1">Category</label>
+            <div className="flex flex-col items-center">
+                <select name="category" className="px-3 py-2 border rounded-md mb-2">
+                <option value="politics">Politics</option>
+                <option value="business">Business</option>
+                <option value="sports">Sports</option>
+                <option value="technology">Technology</option>
+                <option value="entertainment">Entertainment</option>
+                </select>
+                <button type="submit" className="text-white bg-gray-900 font-bold py-2 px-4 rounded">
+                Apply Filters
+                </button>
+            </div>
+        </div>
+
+
+
+    )
 }
 
-function SearchBar() {
-    return
-    <>
-    </>
+function SearchBar({ placeholder = 'Search news...' }) {
+    return (
+        <input
+            type="text"
+            placeholder={placeholder}
+            className="border rounded p-2 shadow focus:outline-none focus:border-black"
+        />
+    );
 }
+
+
 
 function SearchResults() {
+
     return
     <>
     </>
 }
 
-function SearchPage (){
-    return 
+function SearchPage (){ 
+    return (
     <>
         <Navbar />
-        <SearchBar />
+        <SearchBar/>
         <CategorySelection />
         <SearchResults />
         <Footer />
     </>
+    )
 }
 
 export default SearchPage
