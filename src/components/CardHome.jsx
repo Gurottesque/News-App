b598215605fe948758402a2fcb7b0b2466653ff1
Jose Daniel <josedago8@gmail.com>
2024-06-30 22:05:20 -0500

    Adicion Lazyload imagenes

--- src/components/CardHome.jsx
+++ src/components/CardHome.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useGetTrendingHomeQuery } from "../api/NewsApi";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import LazyLoad from 'react-lazyload'
 
 const CardHome = () => {
     const [articles, setArticles] = useState([]);
@@ -16,10 +17,12 @@ const CardHome = () => {
         {articles !== undefined ? articles.map((article) => (
             <Link to={`/about/${article.uri}`} key={article.uri}>
             <section key={article.uri} className="flex flex-1">
-                <article className="shadow-lg mx-auto relative max-w-sm group cursor-pointer">
+                <article className="shadow-lg mx-auto relative max-w-sm group cursor-pointer">      
                     <div className="overflow-hidden">
+                    <LazyLoad offset={100}>
                         <img className="w-full h-auto transform hover:scale-110 duration-200" src={article.images[0]} alt="" />
-                    </div>
+                        </LazyLoad>
+                    </div> 
                     <div className="p-4 my-auto pb-8 overflow-hidden">
                         <p className="text-2xl font-semibold text-gray-800 mt-2">{article.title[Object.keys(article.title)[0]]}</p>
                     </div>
